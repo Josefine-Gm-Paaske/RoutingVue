31abecd2b6e16b9686bcb1ffa5486d68160a750b
Josefine-Gm-Paaske <1086237@ucn.dk>
2022-03-18 12:35:59 +0100

    changed text

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,17 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+        ></a
+      >
     </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
+          ></a
         >
       </li>
       <li>
@@ -23,7 +20,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
-          >router</a
+          ></a
         >
       </li>
       <li>
@@ -31,11 +28,10 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
-          >eslint</a
+          ></a
         >
       </li>
     </ul>
-    <h3>Essential Links</h3>
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
@@ -47,23 +43,22 @@
       </li>
       <li>
         <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
+          > </a
         >
       </li>
       <li>
         <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
+          ></a
         >
       </li>
       <li>
         <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
       </li>
     </ul>
-    <h3>Ecosystem</h3>
     <ul>
       <li>
         <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
+          ></a
         >
       </li>
       <li>
@@ -74,12 +69,12 @@
           href="https://github.com/vuejs/vue-devtools#vue-devtools"
           target="_blank"
           rel="noopener"
-          >vue-devtools</a
+          ></a
         >
       </li>
       <li>
         <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
+          ></a
         >
       </li>
       <li>
@@ -87,7 +82,7 @@
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
           rel="noopener"
-          >awesome-vue</a
+          ></a
         >
       </li>
     </ul>
